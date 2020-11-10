b823d9b01eb719c5ac531027ec5ceb2c1c8fef49
simon <sfeng@scu.edu>
2020-11-09 23:00:24 -0800

    create the post activity

---
+++ app/src/main/java/scu/edu/sharedstyle/Post_item.java
@@ -0,0 +1,14 @@
+package scu.edu.sharedstyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_item);
+    }
+}

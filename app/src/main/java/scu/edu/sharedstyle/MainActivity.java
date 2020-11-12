c3dbe4a89a49887c3bebdcd680150dbb396af8cf
Yiyu Lin <479390737@qq.com>
2020-11-12 11:50:56 +0800

    Product Detail done

--- app/src/main/java/scu/edu/sharedstyle/MainActivity.java
+++ app/src/main/java/scu/edu/sharedstyle/MainActivity.java
@@ -12,10 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
-        }
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow();
+//        }
     }
 }

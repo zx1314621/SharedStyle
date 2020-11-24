af43934008c20932615bbefe5cd841ce3f875570
nstt1234 <nettiesong1@gmail.com>
2020-11-23 19:12:34 -0800

    Improved login process, added google signin

--- app/src/main/java/scu/edu/sharedstyle/activities/LogIn.java
+++ app/src/main/java/scu/edu/sharedstyle/activities/LogIn.java
@@ -9,6 +9,14 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
 import scu.edu.sharedstyle.R;
 
 public class LogIn extends AppCompatActivity {
@@ -18,6 +26,8 @@ public class LogIn extends AppCompatActivity {
 
     EditText username;
     EditText password;
+    int RC_SIGN_IN = 0;
+    private GoogleSignInClient mGoogleSignInClient;
 
 
     @Override
@@ -31,7 +41,68 @@ public class LogIn extends AppCompatActivity {
         // initialize UI elements
         username = findViewById(R.id.et_username);
         password = findViewById(R.id.et_password);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gsignIn();
+            }
+        });
     }
+
+    private void gsignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> task) {
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account!= null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else{
+            Toast.makeText(this, "Sign-in with Google account failed.", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
     public void login(View view) {
         if(! username.getText().toString().equals(USERNAME) || !password.getText().toString().equals(PASSWORD)){
             Toast.makeText(this, "Incorrect Name or Password", Toast.LENGTH_LONG).show();

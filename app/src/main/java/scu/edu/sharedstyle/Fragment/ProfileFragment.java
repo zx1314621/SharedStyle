a51f48bcd0f6b00efa8e60155754743432957100
Yiyu Lin <479390737@qq.com>
2020-11-24 22:05:51 +0800

    Add setting page

--- app/src/main/java/scu/edu/sharedstyle/Fragment/ProfileFragment.java
+++ app/src/main/java/scu/edu/sharedstyle/Fragment/ProfileFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -13,14 +15,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import scu.edu.sharedstyle.R;
+import scu.edu.sharedstyle.activities.AddressActivity;
 import scu.edu.sharedstyle.activities.Front_page;
 import scu.edu.sharedstyle.activities.LogIn;
+import scu.edu.sharedstyle.activities.SettingsActivity;
 
 public class ProfileFragment extends Fragment {
 
 
     private Button resetPW;
     private Button logout;
+    private TableRow tr_address;
+    private TableRow tr_purchased;
+    private TableRow tr_setting;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -29,24 +36,45 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_frofile, container, false);
 
-        resetPW = view.findViewById(R.id.bt_resetPW);
-        resetPW.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Reset Password click", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        resetPW = view.findViewById(R.id.bt_resetPW);
+//        resetPW.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "Reset Password click", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        logout = view.findViewById(R.id.bt_Logout);
+//
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getActivity(), Front_page.class);
+//                Toast.makeText(getActivity(), "Log out", Toast.LENGTH_SHORT).show();
+//                startActivity(intent);
+//            }
+//        });
 
-        logout = view.findViewById(R.id.bt_Logout);
-
-        logout.setOnClickListener(new View.OnClickListener() {
+        tr_setting=view.findViewById(R.id.tr_setting);
+        tr_setting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), Front_page.class);
-                Toast.makeText(getActivity(), "Log out", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), SettingsActivity.class);
                 startActivity(intent);
             }
         });
+
+        tr_address=view.findViewById(R.id.tr_address);
+        tr_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddressActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         return view;
     }
 }
---
+++ app/src/main/java/scu/edu/sharedstyle/activities/AddressActivity.java
@@ -0,0 +1,79 @@
+package scu.edu.sharedstyle.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import scu.edu.sharedstyle.R;
+
+public class AddressActivity extends AppCompatActivity {
+
+
+    private Button btn_cancle;
+    private Button btn_save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle("Address");
+
+        btn_cancle=findViewById(R.id.btn_cancle);
+        btn_cancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancle();
+            }
+        });
+
+        btn_save=findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+    }
+
+    private void cancle(){
+        this.finish();
+    }
+
+    private void save(){
+        //TODO save address to user
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        this.finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right);
+    }
+}

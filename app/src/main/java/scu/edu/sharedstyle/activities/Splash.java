98450da42eb469fe2615b8d3c367ea7792aa8b70
Dylan <liuxiangyu0613@gmail.com>
2020-11-28 21:24:21 -0800

    orientation permission and signin check

--- app/src/main/java/scu/edu/sharedstyle/activities/Splash.java
+++ app/src/main/java/scu/edu/sharedstyle/activities/Splash.java
@@ -6,22 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import scu.edu.sharedstyle.R;
 
 public class Splash extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
+        mAuth = FirebaseAuth.getInstance();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this, Front_page.class);
-                startActivity(i);
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if(currentUser != null){
+                    Intent i = new Intent(Splash.this, MainActivity.class);
+                    startActivity(i);
+
+                } else {
+                    Intent i = new Intent(Splash.this, Front_page.class);
+                    startActivity(i);
+                }
+
 
                 finish();
             }
